db5fbff20a48b780343df27eb602be294b1a2029
harshini-4i <harshinicherry24@gmail.com>
2026-07-30 10:00:48 +0000

    input java program

---
+++ input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Input
+{
+    public static void main(String args[])
+    {
+        int id;
+        Scanner sc = new Scanner(System.in);
+        id=sc.nextInt();
+        System.out.println("my id is:"+id);
+        sc.close();
+    }
+}
