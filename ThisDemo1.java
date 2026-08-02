626b4e585049cdfeeb25c81416dacdbbff4be102
harshini-4i <harshinicherry24@gmail.com>
2026-08-02 18:04:07 +0000

    thisdemo java program

---
+++ ThisDemo1.java
@@ -0,0 +1,20 @@
+class Employee {
+    int id;
+    String name;
+
+    Employee(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    void show() {
+        System.out.println(id + " " + name);
+    }
+}
+
+class ThisDemo1 {
+    public static void main(String args[]) {
+        Employee e1 = new Employee(111, "Harry");
+        e1.show();
+    }
+}
