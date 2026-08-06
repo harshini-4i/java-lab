162d7dee4abf7207e3b79ba1e76cf31d81b62f33
harshini-4i <harshinicherry24@gmail.com>
2026-08-06 08:39:56 +0000

    ArithmeticOperators java program

---
+++ ArithmeticOperators.java
@@ -0,0 +1,13 @@
+class ArithmeticOperators
+{
+public static void main(String[] args)
+{
+int a = 12, b = 5;
+System.out.println("a + b = " + (a + b));
+ System.out.println("a - b = " + (a - b));
+ System.out.println("a * b = " + (a * b));
+ System.out.println("a / b = " + (a / b)); 
+System.out.println("a % b = " + (a % b));
+}
+}
+
