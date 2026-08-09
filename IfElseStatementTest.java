977539de2cdc93fa392479367ec935bce8eb23c3
harshini-4i <harshinicherry24@gmail.com>
2026-08-09 06:18:42 +0000

    IfElseStatementTest java program

---
+++ IfElseStatementTest.java
@@ -0,0 +1,9 @@
+public class IfElseStatementTest
+{
+public static void main(String[] args)
+{
+int a=29; if(a % 2==0)
+System.out.println("Even Number is :"+a); else
+System.out.println("Odd Number is :"+a);
+}
+}
