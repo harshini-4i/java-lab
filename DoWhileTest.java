364f9b99d97d9c59cbdfd489c453b1babab489de
harshini-4i <harshinicherry24@gmail.com>
2026-08-15 15:48:17 +0000

    dowhiletest java program

---
+++ DoWhileTest.java
@@ -0,0 +1,10 @@
+public class DoWhileTest
+{
+public static void main(String[] args)
+{
+int num = 1; do
+{
+System.out.println(num); num++;
+}while(num <= 10); System.out.println("Statement after do-while!");
+}
+}
