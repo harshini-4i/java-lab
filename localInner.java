1107ee10ce012562923fbd3aef1a2ef99a45f380
harshini-4i <harshinicherry24@gmail.com>
2026-08-16 11:07:55 +0000

    localInner java program

---
+++ localInner.java
@@ -0,0 +1,19 @@
+public class localInner
+{
+private int data=30; void display()
+{
+class Local
+{
+void msg()
+{
+System.out.println(data);
+}
+}
+Local l=new Local(); l.msg();
+}
+public static void main(String args[])
+{
+localInner obj=new localInner(); obj.display();
+}
+}
+
