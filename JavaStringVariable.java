de37376ceea171e9ced2d2e29549bec15a0f679f
harshini-4i <harshinicherry24@gmail.com>
2026-08-21 16:16:07 +0000

    while test java program

--- JavaStringVariable.java
+++ JavaStringVariable.java
@@ -4,7 +4,13 @@ public static void main(String[] args)
 {
 String title = "Java Programming";
 String siteName = "String Handling Methods";
- System.out.println("Length of title: " + title.length()); System.out.println("Char at index 3: " + title.charAt(3)); System.out.println("Index of 'T': " + title.indexOf('T')); System.out.println("Empty: " + title.isEmpty()); System.out.println("Equals: " + siteName.equals(title)); System.out.println("Sub-string: " + siteName.substring(9, 14)); System.out.println("Upper case: " + siteName.toUpperCase());
+ System.out.println("Length of title: " + title.length()); 
+ System.out.println("Char at index 3: " + title.charAt(3)); 
+ System.out.println("Index of 'T': " + title.indexOf('T')); 
+ System.out.println("Empty: " + title.isEmpty()); 
+ System.out.println("Equals: " + siteName.equals(title)); 
+ System.out.println("Sub-string: " + siteName.substring(9, 14)); 
+ System.out.println("Upper case: " + siteName.toUpperCase());
 }
 }
 
---
+++ WhileTest.java
@@ -0,0 +1,11 @@
+public class WhileTest
+{
+public static void main(String[] args)
+{
+int num = 1; while(num <= 10)
+{
+System.out.println(num); num++;
+}
+System.out.println("Statement after while!");
+}
+}
